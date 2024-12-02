72be3dc9ca35789843ead269ba2f1b6345f0ecd5
Haseeb Ansari <haseebansari1000@gmail.com>
2024-12-03 03:06:16 +0500

    Day 2 Part 1

--- src/main/java/com/ansari/haseeb/problems_2024/Day2/Day2.java
+++ src/main/java/com/ansari/haseeb/problems_2024/Day2/Day2.java
@@ -3,6 +3,8 @@ package com.ansari.haseeb.problems_2024.Day2;
 import com.ansari.haseeb.Utils.StringUtil;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Day2 {
@@ -11,8 +13,54 @@ public class Day2 {
 
         List<String> lines = StringUtil.readLinesFromFile("src/main/java/com/ansari/haseeb/problems_2024/Day2/input.txt");
 
-        System.out.println("Day 2 Part 1"+ lines);
+
+         /*
+        Day 2 Part 1
+         */
+
+        List<List<Integer>> levels = new ArrayList<>();
+
+        for (String line : lines) {
+            List<Integer> level =  Arrays.stream(line.split(" "))
+                    .map(Integer::parseInt)
+                    .toList();
+
+            levels.add(level);
+        }
+        
+        int problem1Count = 0;
+
+        for (List <Integer> level : levels) {
+                if(isIncreasingOrDecreasing(level)) {
+                    problem1Count++;
+                }
+        }
+
+        System.out.println("Part 1 Answer: " + problem1Count);
+
+    }
+
+    private static boolean isIncreasingOrDecreasing(List<Integer> level) {
+        boolean isIncreasing = true;
+        boolean isDecreasing = true;
+
+        for (int i = 0; i < level.size() - 1; i++) {
+            int diff = level.get(i + 1) - level.get(i);
+
+            if ((diff != 1 && diff != 2 && diff != 3)) {
+                isIncreasing = false;
+            }
 
 
+            if ((diff != -1 && diff != -2 && diff != -3)) {
+                isDecreasing = false;
+            }
+
+            if (!isIncreasing && !isDecreasing) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
